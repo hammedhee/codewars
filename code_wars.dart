fae510fe8fb0c785982a757f0c73e7dd2d762dd1
muhammedsha@123 <muhammednk536@gmail.com>
2024-09-24 10:00:37 +0530

    first coit

--- code_wars.dart
+++ code_wars.dart
@@ -1,1 +1,23 @@
-gi 
+List<String> splitIntoPairs(String str) {
+  List<String> pairs = [];
+  
+  // Loop through the string in steps of 2
+  for (int i = 0; i < str.length; i += 2) {
+    // Check if there is a next character
+    if (i + 1 < str.length) {
+      pairs.add(str[i] + str[i + 1]);
+    } else {
+      // If not, add the last character and an underscore
+      pairs.add(str[i] + '_');
+    }
+  }
+  
+  return pairs;
+}
+
+void main() {
+  String input = "abcde"; // Example input
+  List<String> result = splitIntoPairs(input);
+  
+  print(result); // Output: [ab, cd, e_]
+}
